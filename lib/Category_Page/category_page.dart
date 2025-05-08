bc0ea40ea6eb5525054e5ba6a1efc170dc329113
Arif0388 <arifhussai542@gmail.com>
2025-05-08 10:34:19 +0530

    remove pacakage

--- lib/Category_Page/category_page.dart
+++ lib/Category_Page/category_page.dart
@@ -3,9 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-
-import '../Config/images.dart';
 import '../Doctors_Page/doctors_page.dart';
 import '../Models/category_model.dart';
 
